86a93c050ebf04bb0b8f789d71202d35772be7aa
SpringTwofactorAuthentication <gauravbadola20@gmail.com>
2025-09-16 01:07:50 -0700

    rotateaRRAYS

--- src/ArraysQuestions.java
+++ src/ArraysQuestions.java
@@ -92,6 +92,116 @@ public class ArraysQuestions {
     }
 
 
+    // left rotate array by one --> Brute force approach
+
+    public static void  leftRotateByOne(int arr[], int n){
+
+        int temp[] = new int[n];
+
+        for (int i = 1; i < n; i++ ){
+
+            temp[i-1]  =arr[i];
+        }
+
+        temp[n-1] = arr[0];
+        for (int i = 0; i < n; i++) {
+            System.out.print(temp[i]+" ");
+        }
+    }
+
+
+    // left rotate by one using optimal approach
+    public static void leftRotateOne(int arr[], int n){
+
+        int temp = arr[0];
+
+        for (int i = 0; i < n; i++){
+            arr[i] = arr[i+1];
+        }
+
+        arr[n-1] = temp;
+
+
+         for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+
+    // right rotatate elements
+
+    public static void rotateRight(int arr[], int n, int k){
+
+        if (n==0) return;
+
+        k = k%n;
+
+        if (k > n) return;
+
+
+
+        // declare the temp array of k size
+        int temp[] = new int[k];
+
+        // copy the k lements in temp array
+
+        for (int i = n - k; i < n; i++){
+            temp[i-n+k] = arr[i];
+        }
+
+        //shift the remining elemets
+
+        for (int i = n-k-1; i >=0; i--){
+            arr[i+k] = arr[i];
+        }
+
+        // copy back into original array
+        for (int i = 0; i < k; i++){
+            arr[i] = temp[i];
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+    // leftRotate
+
+    public static void leftRotate(int arr[], int n, int k){
+
+
+        int temp[] = new int[k];
+
+        // copy element into temp array
+
+        for (int i = 0; i < k; i++){
+            temp[i]   = arr[i];
+        }
+
+
+        // shift the remaining elements
+        for (int i  = 0; i < n-k; i++){
+            arr[i] = arr[i+k];
+        }
+
+
+        // copy the elements into temp
+     for (int i = n-k; i < n; i++){
+         arr[i] = temp[i-n+k];
+     }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+
+    }
+
+
+
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int arr[] = {3,4,5,6,7,8,9,9,4,4};
-        int n = arr.length-1;
+        int arr[] = {1,2,3,4,5,6,7};
+
+        int k = 2;
+        int n = arr.length;
 
 
-       System.out.println(ArraysQuestions.findLargestElement(arr,n));
-
-
-       System.out.println();
-        System.out.println("This is the second largest element:"+ArraysQuestions.secondLargestElement(arr,n));
 
 
         System.out.println();
-       System.out.println(ArraysQuestions.isSortedBruteForce(arr,n));
 
-        System.out.println();
+       ArraysQuestions.leftRotate(arr,n,k);
 
-      int k =  ArraysQuestions.removeDuplicates(arr,n);
-
-        System.out.println("The array after removing duplicate elements is ");
-        for (int i = 0; i < k; i++) {
-            System.out.print(arr[i] + " ");
-        }
 
 
 
